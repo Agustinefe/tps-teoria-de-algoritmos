f8df5d69fabbe51855be2c6dd22bc2b7c767772d
Martina Ágata <mpanetta@fi.uba.ar>
2023-05-01 01:31:45 -0300

    Update simulador.py

--- simulador.py
+++ simulador.py
@@ -6,30 +6,8 @@ from contrabando import pasar_aduana
 from generador import cantidad_obtenida_es_correcta, generar_paquetes, generar_multiples_paquetes, generate_packets_and_briberies, fast_generator
 from copy import deepcopy
 
+
 P_global = 1
-
-def ejecucion_unitaria_greedy():
-    productos = ['Vodka', 'Cigarrillos']
-    cantidad_productos = 50
-    paquetes,sobornos_solicitados = generar_paquetes(productos, cantidad_productos)
-    print(f"Paquetes generados: {paquetes}")
-    sobornos_disponibles = solucion(paquetes, sobornos_solicitados)
-    print(f"Soborno disponible: {sobornos_disponibles}")
-
-
-def ejecucion_multiple_greedy():
-    productos = ['Vodka', 'Cigarrillos']
-    cantidad_productos = 50
-    n = 20
-    paquetes_multiples = generar_multiples_paquetes(productos, cantidad_productos, n)
-    for paquetes,sobornos_solicitados in paquetes_multiples:
-        print(f"Paquetes generados: {paquetes}")
-        print(f"Soborno solicitado: {sobornos_solicitados}")
-        cantidad_maxima_es_correcta = cantidad_obtenida_es_correcta(paquetes, cantidad_productos)
-        generacion_correcta = "✅" if cantidad_maxima_es_correcta else "❌"
-        print(f"Generación de paquetes correcta: {generacion_correcta}")
-        sobornos_disponibles = solucion(paquetes, sobornos_solicitados)
-        print(f"Soborno disponible: {sobornos_disponibles}\n")
 
 
 def dp_knapsack(packets, briberies):
@@ -119,7 +97,7 @@ def main():
     results["results"] = do_simulation(config)
     end = time.time()
 
-    print(f"Tiempo que perdiste de tu vida esperando a que termine esta simulacion: {str(datetime.timedelta(seconds=end-start))}")
+    print(f"Tiempo que perdiste de tu vida esperando a que termine esta simulación: {str(datetime.timedelta(seconds=end-start))}")
 
     # Stringificacion del json
     json_dump_results = json.dumps(results, indent=4)
