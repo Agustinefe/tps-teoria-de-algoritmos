8ba21c9d18d7e2e4bd526f0143afe62ef22a01ad
Agustinefe <agustinefe@Agustinefe>
2023-06-03 20:17:54 -0300

    Added brute force algorithm

--- src/algorithms.py
+++ src/algorithms.py
@@ -3,7 +3,40 @@ import time
 
 BIN_CAPACITY = 1
 EPSILON = 0.00000002
-it = 1
+
+def number_to_base(n, b, s):
+    if n == 0:
+        return [0] * s
+    digits = []
+    while n:
+        digits.append(int(n % b))
+        n //= b
+    if len(digits) < s:
+        digits.extend([0] * (s - len(digits)))
+    return digits[::-1]
+
+def bin_packing(T, k, c=1):
+    # T = [0.1, 0.7, 0.3, 0.1, 0.9, 0.4, 0.5]
+    # n = 7, puedo iterar hasta 6
+    # o sea: 2^7 - 1 = 127
+    M = [[] for i in range(k)]
+    s = len(T)
+    for i in range(0, (k ** s) - 1):
+        b = number_to_base(i, k, s)
+        for bin_number,t in zip(b, T):
+            M[bin_number].append(t)
+        
+        if all([sum(bin) <= c for bin in M]):
+            return M
+        else:
+            M = [[] for i in range(k)]
+    return None
+
+def brute_force_solution(T):
+    for i in range(1, len(T) + 1):
+        M = bin_packing(T, i)
+        if M:
+            return M
 
 def count(L):
     return sum(list(map(lambda l: len(l), L)))
@@ -35,9 +68,7 @@ def backtracking_solution(T):
     """
 
     base = math.ceil(sum(T)-EPSILON)
-    global it
     for K in range(base, len(T)+1):
-        it = 1
         bins = [[] for _ in range(K)]
         if (res := backtracking_aux(bins, T, 0)):
             return res
